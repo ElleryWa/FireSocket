7955f0b600ab9bd0b5c9a29ea6d948f15450dae6
Carl Walsh <darthwalsh@gmail.com>
2019-09-05 23:17:18 -0700

    WIP for setting up local firebase

--- firesocket.js
+++ firesocket.js
@@ -1,8 +1,8 @@
 "use strict";
 
-// require("firebase/app");
-// require("firebase/auth");
-// require("firebase/firestore");
+const firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
 
 class FireSocket {
   constructor(address) {
--- spec/firesocketspec.js
+++ spec/firesocketspec.js
@@ -1,6 +1,21 @@
 /* global describe it expect */
 
+/**
+ * @typedef { import("firebase") } firebase
+ */
+
 const FireSocket = require("../firesocket");
+
+const firebase = require("@firebase/testing");
+const app = firebase.initializeTestApp({
+  projectId: "my-project",
+  auth: {uid: "123", email: "name@domain.com"},
+});
+
+const attempt = app.firestore()
+  .collection("colId").doc("docId").get();
+firebase.assertFails(attempt);
+firebase.assertSucceeds(attempt);
 
 describe("firesocket", () => {
   it("address", () => {
